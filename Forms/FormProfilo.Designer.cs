3b55353641204f844c2f6faaa1a46589cb5f421a
diskxo <64780086+diskxo@users.noreply.github.com>
2020-06-24 17:35:56 +0200

    Aggiornamento al form "Valutazioni"

--- Forms/FormProfilo.Designer.cs
+++ Forms/FormProfilo.Designer.cs
@@ -32,7 +32,6 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormProfilo));
             this.btnWindowSize = new System.Windows.Forms.Button();
             this.imageList1 = new System.Windows.Forms.ImageList(this.components);
-            this.ChangePropic = new System.Windows.Forms.Button();
             this.ProPic = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -61,24 +60,16 @@
             this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
             this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
             // 
-            // ChangePropic
-            // 
-            this.ChangePropic.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.ChangePropic.FlatAppearance.BorderSize = 0;
-            this.ChangePropic.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ChangePropic.Image = ((System.Drawing.Image)(resources.GetObject("ChangePropic.Image")));
-            this.ChangePropic.Location = new System.Drawing.Point(175, 12);
-            this.ChangePropic.Name = "ChangePropic";
-            this.ChangePropic.Size = new System.Drawing.Size(40, 37);
-            this.ChangePropic.TabIndex = 5;
-            this.ChangePropic.UseVisualStyleBackColor = true;
-            this.ChangePropic.Click += new System.EventHandler(this.ChangePropic_Click);
-            // 
             // ProPic
             // 
+            this.ProPic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ProPic.ErrorImage = ((System.Drawing.Image)(resources.GetObject("ProPic.ErrorImage")));
+            this.ProPic.Image = ((System.Drawing.Image)(resources.GetObject("ProPic.Image")));
+            this.ProPic.InitialImage = ((System.Drawing.Image)(resources.GetObject("ProPic.InitialImage")));
             this.ProPic.Location = new System.Drawing.Point(12, 12);
             this.ProPic.Name = "ProPic";
-            this.ProPic.Size = new System.Drawing.Size(149, 127);
+            this.ProPic.Size = new System.Drawing.Size(130, 130);
+            this.ProPic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.ProPic.TabIndex = 6;
             this.ProPic.TabStop = false;
             this.ProPic.Click += new System.EventHandler(this.ProPic_Click);
@@ -88,7 +79,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(13, 149);
+            this.label1.Location = new System.Drawing.Point(12, 185);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(74, 19);
             this.label1.TabIndex = 7;
@@ -99,7 +90,7 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft YaHei", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(13, 179);
+            this.label2.Location = new System.Drawing.Point(12, 223);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(82, 19);
             this.label2.TabIndex = 8;
@@ -112,7 +103,7 @@
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(101, 150);
+            this.textBox1.Location = new System.Drawing.Point(100, 185);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(158, 23);
             this.textBox1.TabIndex = 9;
@@ -126,7 +117,7 @@
             this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox2.Font = new System.Drawing.Font("Microsoft YaHei", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox2.ForeColor = System.Drawing.Color.White;
-            this.textBox2.Location = new System.Drawing.Point(101, 180);
+            this.textBox2.Location = new System.Drawing.Point(100, 223);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(224, 23);
             this.textBox2.TabIndex = 10;
@@ -144,10 +135,9 @@
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.ProPic);
-            this.Controls.Add(this.ChangePropic);
             this.Controls.Add(this.btnWindowSize);
             this.Name = "FormProfilo";
-            this.Text = "PROFILO";
+            this.Text = "Profilo";
             this.Load += new System.EventHandler(this.FormProfilo_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ProPic)).EndInit();
             this.ResumeLayout(false);
@@ -158,7 +148,6 @@
         #endregion
         private System.Windows.Forms.Button btnWindowSize;
         private System.Windows.Forms.ImageList imageList1;
-        private System.Windows.Forms.Button ChangePropic;
         private System.Windows.Forms.PictureBox ProPic;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
