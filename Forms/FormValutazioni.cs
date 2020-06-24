a99c17392b13bb93787e09b7f08bd237c96c7224
diskxo <64780086+diskxo@users.noreply.github.com>
2020-06-24 17:36:06 +0200

    Aggiornamento al form "Valutazioni"

--- Forms/FormValutazioni.cs
+++ Forms/FormValutazioni.cs
@@ -12,6 +12,8 @@ namespace Journal_Elite.Forms
 {
     public partial class FormValutazioni : Form
     {
+        System.Data.DataTable dt = new System.Data.DataTable();
+        DataTable table = new DataTable("tbl");
         public FormValutazioni()
         {
             InitializeComponent();
@@ -25,8 +27,81 @@ namespace Journal_Elite.Forms
 
         private void AddSubject_Click(object sender, EventArgs e)
         {
-            AddSubjectForm f2 = new AddSubjectForm();
-            f2.Show();
+            lblName.Visible = true;
+            txtName.Visible = true;
+            SaveSubject.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SaveSubject_Click(object sender, EventArgs e)
+        {
+
+            if (label1.Visible == false)
+            {
+                label1.Visible = true;
+                bunifuCircleProgressbar1.Visible = true;
+                AddVote.Visible = true;
+                label1.Text = txtName.Text;
+            }
+            else if(label2.Visible == false)
+            {
+                label2.Visible = true;
+                bunifuCircleProgressbar2.Visible = true;
+                button1.Visible = true;
+            }
+            else if (label3.Visible == false)
+            {
+                label3.Visible = true;
+                bunifuCircleProgressbar3.Visible = true;
+                button2.Visible = true;
+            }
+            else if (label4.Visible == false)
+            {
+                label4.Visible = true;
+                bunifuCircleProgressbar4.Visible = true;
+                button3.Visible = true;
+            }
+            else if (label5.Visible == false)
+            {
+                label5.Visible = true;
+                bunifuCircleProgressbar5.Visible = true;
+                button4.Visible = true;
+            }
+            else if (label6.Visible == false)
+            {
+                label6.Visible = true;
+                bunifuCircleProgressbar6.Visible = true;
+                button5.Visible = true;
+            }
+            else if (label7.Visible == false)
+            {
+                label7.Visible = true;
+                bunifuCircleProgressbar7.Visible = true;
+                button6.Visible = true;
+            }
+            else if (label8.Visible == false)
+            {
+                label8.Visible = true;
+                bunifuCircleProgressbar8.Visible = true;
+                button7.Visible = true;
+            }
+            else if (label9.Visible == false)
+            {
+                label9.Visible = true;
+                bunifuCircleProgressbar9.Visible = true;
+                button8.Visible = true;
+            }
+            else if (label10.Visible == false)
+            {
+                label10.Visible = true;
+                bunifuCircleProgressbar10.Visible = true;
+                button9.Visible = true;
+            }
+
         }
     }
     
