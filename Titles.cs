73d691c9cba6f2543fb7be168a656430f07d9cc9
Your Namediskxo <you@example.com>
2020-08-05 11:11:02 +0200

    Aggiornamento grafico

---
+++ Titles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Journal_Elite
+{
+    public class Titles
+    {
+        public string Name { get; set; }
+        public int Class { get; set; }
+        public string sub { get; set; }
+    }
+}
